a27a34c524d53518a46e1ad230699a18bf31cb2f
“wanghongyuu” <1520583457@qq.com>
2022-10-20 15:56:13 +0800

    Site updated: 2022-10-20 15:56:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/4a17b156.html","posts/6b185cce.html","posts/5938ac41.html","posts/6e952047.html","posts/0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/6e952047.html","posts/6b185cce.html","posts/5938ac41.html","posts/0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
